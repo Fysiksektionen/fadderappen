8a0e61c997bcf0993103720ca49bcb37612d0a59
Calle Svensson <calle.svensson@zeta-two.com>
2015-08-17 00:29:13 +0200

    Minor fixes

--- standardglas.php
+++ standardglas.php
@@ -3,18 +3,14 @@
 <div class="row">
     <div class="large-12 columns">
         <h1>Mängden alkohol på fester</h1>
-        <p>
-            Servera aldrig mer än <strong>3 standardglas</strong> med en alkbiljett. Det är rekommenderat maxintag för kvinnor.
-        </p>
-        <p>
-            Exempel på ett standardglas:
-            <ul>
-                <li>33cl starköl (&asymp; 5%)</li>
-                <li>50cl folköl (&asymp; 3.5%)</li>
-                <li>15cl (ett glas) vin (&asymp; 11-15%)</li>
-                <li>4cl starksprit (&asymp; 40%, punsch är ca 25%)</li>
-            </ul>
-            Sittningsexempel: 2st öl/cider (33cl, 5% eller 50 cl <= 3.5%) + 2cl starksprit (40%) + 3cl punsch (22%)
-        </p>
+        <p>Servera aldrig mer än <strong>3 standardglas</strong> med en alkbiljett. Det är rekommenderat maxintag för kvinnor.</p>
+        <p>Exempel på ett standardglas:</p>
+        <ul>
+            <li>33cl starköl (&asymp; 5%)</li>
+            <li>50cl folköl (&asymp; 3.5%)</li>
+            <li>15cl (ett glas) vin (&asymp; 11-15%)</li>
+            <li>4cl starksprit (&asymp; 40%, punsch är ca 25%)</li>
+        </ul>
+        <p>Sittningsexempel: 2st öl/cider (33cl, 5% eller 50 cl &lt= 3.5%) + 2cl starksprit (40%) + 3cl punsch (22%)</p>
     </div>
 </div>
