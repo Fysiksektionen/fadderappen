b91117e2b5c9af8896e27b8dd81ece7d376999e3
Calle Svensson <calle.svensson@zeta-two.com>
2015-08-16 20:48:39 +0200

    Added schedule structure

--- schema.php
+++ schema.php
@@ -2,8 +2,99 @@
 
 <div class="row">
   <div class="large-12 columns">
-    <h1>Fadderappen</h1>
-    <p>Välkommen till infosidan</p>
+    <h1>Schema</h1>
+    <ul class="accordion" data-accordion>
+	  <li class="accordion-navigation">
+	    <a href="#schema1">Måndag 17/8</a>
+	    <div id="schema1" class="content active">
+	      Panel 1. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	    </div>
+	  </li>
+	  <li class="accordion-navigation">
+	    <a href="#schema2">Tisdag 18/8</a>
+	    <div id="schema2" class="content">
+	      Panel 2. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	    </div>
+	  </li>
+	  <li class="accordion-navigation">
+	    <a href="#schema3">Onsdag 19/8</a>
+	    <div id="schema3" class="content">
+	      Panel 3. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	    </div>
+	  </li>
+	  <li class="accordion-navigation">
+	    <a href="#schema4">Torsdag 20/8</a>
+	    <div id="schema4" class="content">
+	      Panel 3. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	    </div>
+	  </li>
+	  <li class="accordion-navigation">
+	    <a href="#schema5">Fredag 21/8</a>
+	    <div id="schema5" class="content">
+	      Panel 3. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	    </div>
+	  </li>
+	  <li class="accordion-navigation">
+	    <a href="#schema6">Lördag 22/8</a>
+	    <div id="schema6" class="content">
+	      Panel 3. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	    </div>
+	  </li>
+	  <li class="accordion-navigation">
+	    <a href="#schema7">Söndag 23/8</a>
+	    <div id="schema7" class="content">
+	      Panel 3. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	    </div>
+	  </li>
+	  <li class="accordion-navigation">
+	    <a href="#schema8">Måndag 24/8</a>
+	    <div id="schema8" class="content">
+	      Panel 3. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	    </div>
+	  </li>
+	  <li class="accordion-navigation">
+	    <a href="#schema9">Tisdag 25/8</a>
+	    <div id="schema9" class="content">
+	      Panel 3. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	    </div>
+	  </li>
+	  <li class="accordion-navigation">
+	    <a href="#schema10">Onsdag 26/8</a>
+	    <div id="schema10" class="content">
+	      Panel 3. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	    </div>
+	  </li>
+	  <li class="accordion-navigation">
+	    <a href="#schema11">Torsdag 27/8</a>
+	    <div id="schema11" class="content">
+	      Panel 3. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	    </div>
+	  </li>
+	  <li class="accordion-navigation">
+	    <a href="#schema12">Fredag 28/8</a>
+	    <div id="schema12" class="content">
+	      Panel 3. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	    </div>
+	  </li>
+	  <li class="accordion-navigation">
+	    <a href="#schema13">Lördag 29/8</a>
+	    <div id="schema13" class="content">
+	      Panel 3. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	    </div>
+	  </li>
+	  <li class="accordion-navigation">
+	    <a href="#schema14">Måndag 31/8</a>
+	    <div id="schema14" class="content">
+	      Panel 3. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	    </div>
+	  </li>
+	  <li class="accordion-navigation">
+	    <a href="#schema15">Tisdag 1/9</a>
+	    <div id="schema15" class="content">
+	      Panel 3. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	    </div>
+	  </li>
+	</ul>
   </div>
 </div>
 
