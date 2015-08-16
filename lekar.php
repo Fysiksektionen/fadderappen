098621884c637b5807e2de5a1094e12bfec1b779
Tomas Lycken <tomas.lycken@gmail.com>
2015-08-16 21:07:04 +0200

    lekar

---
+++ lekar.php
@@ -0,0 +1,74 @@
+<?php $title = 'Lekar'; include('template/head.php'); ?>
+
+<div class="row">
+  <div class="large-12 columns">
+    <h1>Lekar</h1>
+    <ul class="accordion" data-accordion>
+      <li class="accordion-navigation">
+        <a href="#fotleken">Fotleken</a>
+        <div id="fotleken" class="content">
+          <p>
+            Alla står i en cirkel med fötterna mot grannen. En börjar och lyfter ena foten så att den rör vid andra grannens fot (dvs så att båda personens fötter rör vid ena grannens båda fötter).Grannen gör sedan samma sak och så fortsätter det varvet runt.
+          </p>
+          <p>
+            Regler:
+            <ul>
+              <li>Foten man flyttar får inte sättas så att den rör vid en fot som man redan vidrör.</li>
+              <li>Händer får inte vidröra marken.</li>
+              <li>Det är alltid samma person man flyttar fötterna till (alltså inte alltid den som är närmst).</li>
+              <li>Om man faller eller inte lyckas sätta foten intill grannens så åker man ut.</li>
+              <li>Då får nästa på tur välja vilken fot hen vill flytta.</li>
+              <li>Sista två kvar vinner!</li>
+            </ul>
+          </p>
+        </div>
+      </li>
+      <li class="accordion-navigation">
+        <a href="#ninja">Ninja</a>
+        <div id="ninja" class="content">
+          <p>
+            Alla står i en cirkel med händerna mot varandra. På given
+            signal hoppas alla bakåt. Man får nu i tur och ordning göra en rörelse (hoppa/gå/röra armarna). Målet är att slå de andra deltagarna på händerna. Om man är målet för en attack får man försvara sig genom att i en rörelse försöka fly. Lyckas attacken åker motspelaren ut. Sist kvar vinner!
+          </p>
+        </div>
+      </li>
+      <li class="accordion-navigation">
+        <a href="#samuraj">Samuraj</a>
+        <div id="samuraj" class="content">
+          <p>
+            Alla står i en cirkel. En börjar och skickar iväg en attack
+            mot valfri deltagare. Den som attackeras svarar med en
+            försvarsrörelse. Detta följs upp av en reaktion från offrets
+            grannar. Om offret reagerar rätt får hen skicka nästa
+            attack. Attacker, försvar och reaktioner är i förväg
+            bestämda rörelser och ljud. Dessa är grupperade och
+            fungerar bara om de görs korrekt och till rätt attack. Gör
+            man fel rörelse/ljud eller om man inte agerar tillräckligt
+            snabbt åker man ut. Sist kvar vinner!
+          </p>
+        </div>
+      </li>
+      <li class="accordion-navigation">
+        <a href="#knuten">Knuten</a>
+        <div id="knuten" class="content">
+          <p>
+            Alla står i en cirkel. På given signal blundar alla och går
+            mot mitten med utsträckta armar. När alla fått tag på en
+            hand i båda händerna öppnar alla ögonen. Uppgiften är nu
+            att lösa upp knuten, utan att släppa händerna.
+          </p>
+        </div>
+      </li>
+      <li class="accordion-navigation">
+        <a href="#svensk-tunnelbana">Svensk tunnelbana</a>
+        <div id="svensk-tunnelbana" class="content">
+          <p>
+            Alla står i en cirkel och blundar. På en given signal tittar alla på någon annan i cirkeln. Den som tittar in i någon annans ögon skriker, och är ute i leken. Sist kvar vinner!
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</div>
+
+<?php include('template/footer.php'); ?>
