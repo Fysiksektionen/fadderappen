46252c6284f7dc22de7535f1e34cb6f944171c5c
Calle Svensson <calle.svensson@zeta-two.com>
2015-08-16 16:41:49 +0200

    Added basic template system

---
+++ schema.php
@@ -0,0 +1,10 @@
+<?php $title = 'Schema'; include('template/head.php'); ?>
+
+<div class="row">
+  <div class="large-12 columns">
+    <h1>Fadderappen</h1>
+    <p>Välkommen till infosidan</p>
+  </div>
+</div>
+
+<?php include('template/footer.php'); ?>
---
+++ template/head.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Fadderappen<?php if(isset($title)) { echo " - $title"; }?></title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+    
+    <nav class="top-bar" data-topbar role="navigation">
+      <ul class="title-area">
+        <li class="name">
+          <h1><a href="#">Fadderappen</a></h1>
+        </li>
+         <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+        <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+      </ul>
+
+      <section class="top-bar-section">
+        <!-- Left Nav Section -->
+        <ul class="left">
+          <li><a href="#">Infosida 1</a></li>
+          <li><a href="#">Infosida 2</a></li>
+        </ul>
+      </section>
+    </nav>
