e2bd5b6341ccdead464ea8227c5027421fc2ce0f
Tomas Lycken <tomas.lycken@gmail.com>
2015-08-16 21:53:56 +0200

    kirs

--- kris.php
+++ kris.php
@@ -1,11 +1,10 @@
 <?php $title = 'Kris'; include('template/head.php'); ?>
 
-    <div class="row">
-      <div class="large-12 columns">
+<div class="row">
+    <div class="large-12 columns">
         <h1 class="big">Är situationen akut? <strong>RING 112</strong></h1>
-        <h2>Krisplan</h2>
-        
-      </div>
+        <p>Läs <a href="https://docs.google.com/document/d/1NpzSma3nJl9UsDd2WHdzISNJQXBi3eWz7_zTz6BXASM/edit?usp=sharing">krisplanen</a>.
     </div>
-    
+</div>
+
 <?php include('template/footer.php'); ?>
